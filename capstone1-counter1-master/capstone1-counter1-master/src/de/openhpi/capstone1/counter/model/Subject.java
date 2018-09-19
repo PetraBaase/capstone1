8a183a5ba42a9abf472bac024da19f3a9f0f0363
PetraBaase <petra@baase-hueskes.de>
2018-09-19 18:13:26 +0200

    first

--- capstone1-counter1-master/capstone1-counter1-master/src/de/openhpi/capstone1/counter/model/Subject.java
+++
@@ -1,8 +0,0 @@
-package de.openhpi.capstone1.counter.model;
-
-import de.openhpi.capstone1.counter.view.Observer;
-
-public interface Subject {
-	void attach(Observer observer);
-	void notifyAllObservers();
-}
